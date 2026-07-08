b8c05c7c9a31470a80ad294a98e1c8fad17687dc
Rowena091 <Melwenwaller@gmail.com>
2026-07-07 23:56:58 -0700

    StickerSmash

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -1,5 +1,10 @@
-import { Stack } from "expo-router";
+import { Tabs } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen name="about" options={{ title: 'About' }} />
+    </Tabs>
+  );
 }
